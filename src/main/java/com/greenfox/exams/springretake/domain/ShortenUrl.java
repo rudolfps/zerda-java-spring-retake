497b5d33e06edb7731f74df68149ff78782f31c1
rudolfps <rudolfps@gmail.com>
2017-01-23 09:53:53 +0100

    setting up the domain

---
+++ src/main/java/com/greenfox/exams/springretake/domain/ShortenUrl.java
@@ -0,0 +1,22 @@
+package com.greenfox.exams.springretake.domain;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by ${rudolfps} on 2017.01.23..
+ */
+@Entity
+@Table(name="urlstore")
+@Data
+public class ShortenUrl {
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+        @NotNull
+        private String url;
+    }
+
